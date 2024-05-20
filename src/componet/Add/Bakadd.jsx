88618b5855d78d323b2e0f6d69b8bd37352332c5
dev <166125538+Purohitdev@users.noreply.github.com>
2024-05-20 11:48:21 +0530

    Update Bakadd.jsx

--- src/componet/Add/Bakadd.jsx
+++ src/componet/Add/Bakadd.jsx
@@ -3,6 +3,8 @@ import { Formik } from 'formik';
 import axios from 'axios';
 import { Navigate, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 
 
