e85abe1c0f4ddea610e870f380695520eae0e046
dev <166125538+Purohitdev@users.noreply.github.com>
2024-05-20 12:45:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../src/Home.css"
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+
 
 
 
@@ -14,18 +13,6 @@ root.render(
   <React.StrictMode>
     <>
       <App />
-      <ToastContainer
-      position="top-right"
-      autoClose={3000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="light"
-       />
 
     </>
   </React.StrictMode>
